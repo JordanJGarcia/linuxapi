14eb081ef77d2e3573023f82ef8ee13b5c2d7416
Jordan Garcia <jordan.garcia@lowes.com>
2023-02-14 11:42:37 -0500

    Include my own error logging module

---
+++ lib/error.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifndef ERROR_H
+#define ERROR_H
+
+/* libraries */
+#include <stdio.h>  /* stdio functions */
+#include <string.h> /* string functions */
+#include <stdarg.h> /* uh.... */
+#include <errno.h>  /* errno and error constants */
+
+/* macros */
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifdef __GNUC__                                 // This stops 'gcc -Wall' from complaining that
+#define NORETURN __attribute__ ((__noreturn__)) // "control reaches end of non-void function"
+#else
+#define NORETURN
+#endif
+
+
+// errno is statically allocated, so does not need to be passed to these macro functions
+#define LOG_ERROR(file, format, ...) \
+    do                                                              \
+    {                                                               \
+        fprintf(file, format,  ##__VA_ARGS__);                      \
+        fprintf(file, "file: %s\tline: %d\n", __FILE__, __LINE__);  \
+        fprintf(file, "Errno: %s\n", strerror(errno));              \
+    }                                                               \
+    while(0)                                                    
+
+#define LOG_ERROR_EXIT(status, file, format, ...) \
+    do                                                              \
+    {                                                               \
+        fprintf(file, format,  ##__VA_ARGS__);                      \
+        fprintf(file, "file: %s\tline: %d\n", __FILE__, __LINE__);  \
+        fprintf(file, "Errno: %s\n", strerror(errno));              \
+        exit(status);                                               \
+    }                                                               \
+    while(0)                                                    
+
+#endif
